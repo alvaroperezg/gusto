416f4b22e5924e7657494f73f97e24e792faba76
Javier Alaves <jayalaves@gmail.com>
2023-11-22 23:47:14 +0100

    add ppl images

--- src/screens/NewPlanScreen.js
+++ src/screens/NewPlanScreen.js
@@ -1,12 +1,68 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 const NewPlanScreen = ({ navigation }) => {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  };
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.screen}>
         <View style={styles.content}>
-          <Text>Vamos a crear un plan</Text>
+          {/* Card */}
+          <View style={styles.card}>
+            <View style={styles.datePickerContainer}>
+              <DateTimePicker
+                value={date}
+                mode="date"
+                display="default"
+                onChange={onChange}
+              />
+            </View>
+
+            {/* Horizontal Row 1 */}
+            <View style={styles.row}>
+              <Text style={styles.rowText}>Comida</Text>
+              <View style={styles.imageContainer}>
+                <Image
+                  source={require("../../assets/ppl/javi.jpg")}
+                  style={styles.circleImage}
+                />
+                <Image
+                  source={require("../../assets/ppl/diego.jpg")}
+                  style={styles.circleImage}
+                />
+                <Image
+                  source={require("../../assets/ppl/alvaro.jpg")}
+                  style={styles.circleImage}
+                />
+              </View>
+            </View>
+
+            {/* Horizontal Row 2 */}
+            <View style={styles.row}>
+              <Text style={styles.rowText}>Cena</Text>
+              <View style={styles.imageContainer}>
+                <Image
+                  source={require("../../assets/ppl/javi.jpg")}
+                  style={styles.circleImage}
+                />
+                <Image
+                  source={require("../../assets/ppl/diego.jpg")}
+                  style={styles.circleImage}
+                />
+                <Image
+                  source={require("../../assets/ppl/alvaro.jpg")}
+                  style={styles.circleImage}
+                />
+              </View>
+            </View>
+          </View>
         </View>
       </View>
     </SafeAreaView>
@@ -24,6 +80,36 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     padding: 20,
+  },
+  card: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  datePickerContainer: {
+    alignItems: "flex-start",
+    marginLeft: -10,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 12,
+  },
+  rowText: {
+    flex: 1,
+    fontSize: 16,
+  },
+  imageContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  circleImage: {
+    width: 32,
+    height: 32,
+    borderRadius: 16,
+    marginLeft: 10,
+    overflow: "hidden",
   },
 });
 
