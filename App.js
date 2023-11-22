e744b22774c41cec1b020e5b8f89346331aad1a2
Javier Alaves <jayalaves@gmail.com>
2023-11-22 19:44:43 +0100

    navigation to plans screen

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// pantallas a importar para navegación
+import PlansScreen from "./src/screens/PlansScreen";
 
 export default function App() {
+  // para navegación entre pantallas
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>¿Estás a gusto?</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Plans">
+        <Stack.Screen name="Planes" component={PlansScreen} />
+        {/* otras pantallas van aquí */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/PlansScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const PlansScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Estos son los planes, tetes</Text>
+    </View>
+  );
+};
+
+export default PlansScreen;
+
+const styles = StyleSheet.create({});
