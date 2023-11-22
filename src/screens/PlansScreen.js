78e115c9160e96a3fa2f16bf4ea5017451d73e32
Javier Alaves <jayalaves@gmail.com>
2023-11-22 19:53:25 +0100

    add plus button

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,14 +1,57 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const PlansScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Estos son los planes, tetes</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.screen}>
+        <View style={styles.content}>
+          <Text>No hay planes, tetes</Text>
+        </View>
+
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => {
+            /* esto me tiene que llevar a la pantalla de crear un plan */
+          }}
+        >
+          <Ionicons name="add" size={24} color="white" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default PlansScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "lightgray",
+  },
+  screen: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    padding: 20,
+  },
+  addButton: {
+    position: "absolute",
+    right: 32,
+    bottom: 32,
+    backgroundColor: "green",
+    width: 64,
+    height: 64,
+    borderRadius: 999,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
