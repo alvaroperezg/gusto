feb12aea2550dfba4df065948493562818ee7ec7
Javier Alaves <jayalaves@gmail.com>
2023-11-23 20:01:14 +0100

    remove hardcoded cards

---
+++ src/components/Plans/PlanCard.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import TouchableImage from "./TouchableImage";
+
+const PlanCard = ({ initialDate }) => {
+  const [date, setDate] = useState(initialDate || new Date());
+  const [lunchCount, setLunchCount] = useState(3);
+  const [dinnerCount, setDinnerCount] = useState(3);
+
+  const onChangeDate = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  };
+
+  const handleLunchToggle = (isActive) => {
+    setLunchCount((prevCount) => (isActive ? prevCount + 1 : prevCount - 1));
+  };
+
+  const handleDinnerToggle = (isActive) => {
+    setDinnerCount((prevCount) => (isActive ? prevCount + 1 : prevCount - 1));
+  };
+
+  return (
+    <View style={styles.card}>
+      <View style={styles.datePickerContainer}>
+        {/* check to make sure there's a date in state */}
+        {date && (
+          <DateTimePicker
+            value={date}
+            mode="date"
+            display="default"
+            onChange={onChangeDate}
+          />
+        )}
+      </View>
+
+      <View style={styles.row}>
+        <Text style={styles.rowText}>Comida</Text>
+        <View style={styles.imageContainer}>
+          <TouchableImage
+            source={require("../../../assets/ppl/javi.jpg")}
+            onToggle={handleLunchToggle}
+          />
+          <TouchableImage
+            source={require("../../../assets/ppl/diego.jpg")}
+            onToggle={handleLunchToggle}
+          />
+          <TouchableImage
+            source={require("../../../assets/ppl/alvaro.jpg")}
+            onToggle={handleLunchToggle}
+          />
+        </View>
+      </View>
+
+      <View style={styles.row}>
+        <Text style={styles.rowText}>Cena</Text>
+        <View style={styles.imageContainer}>
+          <TouchableImage
+            source={require("../../../assets/ppl/javi.jpg")}
+            onToggle={handleDinnerToggle}
+          />
+          <TouchableImage
+            source={require("../../../assets/ppl/diego.jpg")}
+            onToggle={handleDinnerToggle}
+          />
+          <TouchableImage
+            source={require("../../../assets/ppl/alvaro.jpg")}
+            onToggle={handleDinnerToggle}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  datePickerContainer: {
+    alignItems: "flex-start",
+    marginLeft: -10,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 12,
+  },
+  rowText: {
+    flex: 1,
+    fontSize: 16,
+  },
+  imageContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+});
+
+export default PlanCard;
--- src/screens/NewPlanScreen.js
+++ src/screens/NewPlanScreen.js
@@ -1,71 +1,19 @@
-import {
-  Image,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { SafeAreaView, StyleSheet, TouchableOpacity, View } from "react-native";
 import React, { useState } from "react";
-import DateTimePicker from "@react-native-community/datetimepicker";
-import TouchableImage from "../components/Plans/TouchableImage";
 import { Ionicons } from "@expo/vector-icons";
+import PlanCard from "../components/Plans/PlanCard";
 
 const NewPlanScreen = ({ navigation }) => {
-  const [date, setDate] = useState(new Date());
-
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setDate(currentDate);
-  };
-
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.screen}>
         <View style={styles.content}>
-          {/* Card */}
-          <View style={styles.card}>
-            <View style={styles.datePickerContainer}>
-              <DateTimePicker
-                value={date}
-                mode="date"
-                display="default"
-                onChange={onChange}
-              />
-            </View>
+          <PlanCard initialDate={new Date()} />
 
-            {/* Horizontal Row 1 */}
-            <View style={styles.row}>
-              <Text style={styles.rowText}>Comida</Text>
-              <View style={styles.imageContainer}>
-                <TouchableImage source={require("../../assets/ppl/javi.jpg")} />
-                <TouchableImage
-                  source={require("../../assets/ppl/diego.jpg")}
-                />
-                <TouchableImage
-                  source={require("../../assets/ppl/alvaro.jpg")}
-                />
-              </View>
-            </View>
-
-            {/* Horizontal Row 2 */}
-            <View style={styles.row}>
-              <Text style={styles.rowText}>Cena</Text>
-              <View style={styles.imageContainer}>
-                <TouchableImage source={require("../../assets/ppl/javi.jpg")} />
-                <TouchableImage
-                  source={require("../../assets/ppl/diego.jpg")}
-                />
-                <TouchableImage
-                  source={require("../../assets/ppl/alvaro.jpg")}
-                />
-              </View>
-            </View>
-          </View>
+          {/* Button to add another plan card */}
           <TouchableOpacity
             style={styles.addButton}
             onPress={() => {
-              // Handle the button press here
               console.log("Add Date button pressed");
             }}
           >
@@ -88,36 +36,6 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     padding: 20,
-  },
-  card: {
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
-    marginBottom: 20,
-  },
-  datePickerContainer: {
-    alignItems: "flex-start",
-    marginLeft: -10,
-  },
-  row: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: 12,
-  },
-  rowText: {
-    flex: 1,
-    fontSize: 16,
-  },
-  imageContainer: {
-    flexDirection: "row",
-    justifyContent: "flex-end",
-  },
-  circleImage: {
-    width: 32,
-    height: 32,
-    borderRadius: 16,
-    marginLeft: 10,
-    overflow: "hidden",
   },
   addButton: {
     backgroundColor: "gray",
