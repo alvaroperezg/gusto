60000798b47f34edf7aac58189711629410f5d23
Javier Alaves <jayalaves@gmail.com>
2023-11-25 15:48:16 +0100

    date picker on ios and android

--- src/components/Plans/PlanCard.js
+++ src/components/Plans/PlanCard.js
@@ -1,12 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import DateTimePicker from "@react-native-community/datetimepicker";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 import TouchableImage from "./TouchableImage";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 
 const PlanCard = ({ plan, onPlanChange }) => {
+  const [date, setDate] = useState(plan.date);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
   const onChangeDate = (event, selectedDate) => {
-    const updatedPlan = { ...plan, date: selectedDate || plan.date };
-    onPlanChange(updatedPlan);
+    const currentDate = selectedDate || date;
+    setShowDatePicker(Platform.OS === "ios"); // for iOS, we need to control the visibility
+    setDate(currentDate); // Update the local state
+    onPlanChange({ ...plan, date: currentDate }); // Update the parent state
+  };
+
+  const showDatePickerAndroid = () => {
+    DateTimePickerAndroid.open({
+      value: date,
+      onChange: onChangeDate,
+      mode: "date",
+      is24Hour: true,
+    });
+  };
+
+  const showDatePickerIOS = () => {
+    setShowDatePicker(true);
   };
 
   const handleLunchToggle = (isActive) => {
@@ -21,12 +46,28 @@ const PlanCard = ({ plan, onPlanChange }) => {
     onPlanChange(updatedPlan);
   };
 
+  const formatDate = (date) => {
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
+
   return (
     <View style={styles.card}>
       <View style={styles.datePickerContainer}>
-        {plan.date && (
+        {Platform.OS === "android" ? (
+          <TouchableOpacity
+            style={styles.button}
+            onPress={showDatePickerAndroid}
+          >
+            {/* Display the formatted date on the button */}
+            <Text style={styles.buttonText}>{formatDate(date)}</Text>
+          </TouchableOpacity>
+        ) : (
           <DateTimePicker
-            value={plan.date}
+            value={date}
             mode="date"
             display="default"
             onChange={onChangeDate}
@@ -96,6 +137,15 @@ const styles = StyleSheet.create({
   imageContainer: {
     flexDirection: "row",
     justifyContent: "flex-end",
+  },
+  button: {
+    backgroundColor: "#007AFF",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
   },
 });
 
